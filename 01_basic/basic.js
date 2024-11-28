8326def0a58c7f32106d780adb6c9daacd8470dd
Himanshu <140635492+HimanshuGhorai@users.noreply.github.com>
2024-11-28 16:20:38 +0000

    add now notes for js

---
+++ 01_basic/basic.js
@@ -0,0 +1,16 @@
+/* jsva script basic to advanced 
+
+javaScript *
+
+what is javaScript ?
+javascript is a lightweight interpreted scripting language use to a dynamic and interative content and dinamic web applictions
+
+key Feature*
+
+platform-independent 
+object-oriented 
+Event-driven 
+
+
+
+*/
